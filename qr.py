7049bb144f9a8af9fddcfd868b917bb2ce9fb0ce
vandana2003gupta <vandana2003gupta@gmail.com>
2024-10-13 14:19:34 +0530

    Update qr.py

--- qr.py
+++ qr.py
@@ -3,8 +3,8 @@ from PIL import Image
 
 qr = qrcode.QRCode(version =1, error_correction=qrcode.constants.ERROR_CORRECT_H, box_size = 10,border=4,)
 
-qr.add_data("https://i.im.ge/2024/10/13/kwJ9Fa.1000017266.jpeg")
+qr.add_data("https://i.im.ge/2024/10/13/kwJj9J.1000017147.jpeg")
 qr.make(fit= True)
 
 img = qr.make_image(fill_color ="Black",black_color="white")
-img.save("divya.png")
+img.save("qr.png")
